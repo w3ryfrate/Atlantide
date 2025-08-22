5ef71599ae04b013ccd55813fbe8cdb3b2e2793b
Weryfrate <w3ryfrate@gmail.com>
2025-08-23 00:16:33 +0200

    Loggers

---
+++ ConsoleLogger.cs
@@ -0,0 +1,36 @@
+﻿
+using Serilog;
+
+namespace ProjectNewWorld.Core;
+
+public class ConsoleLogger : ILogger
+{
+    private readonly Serilog.ILogger _internalLogger;
+
+    public ConsoleLogger()
+    {
+        _internalLogger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+    }
+
+    public void LogFatal(string message, bool exit = true)
+    {
+        _internalLogger.Fatal(message);
+        if (exit)
+            Environment.Exit(1);
+    }
+
+    public void LogError(string message)
+    {
+        _internalLogger.Error(message);
+    }
+
+    public void LogInformation(string message)
+    {
+        _internalLogger.Information(message);
+    }
+
+    public void LogWarning(string message)
+    {
+        _internalLogger.Warning(message);
+    }
+}
---
+++ ILogger.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ProjectNewWorld.Core;
+
+public interface ILogger
+{
+    void LogFatal(string message, bool exit = true);
+    void LogError(string message);
+    void LogWarning(string message);
+    void LogInformation(string message);
+}
